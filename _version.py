6211b83d17f284c718f790dc105f62565b05bde5
Jason Brodkey <jasonbrodkey@Mac.attlocal.net>
2025-12-10 15:27:25 -0800

    chore: bump version to 1.0.1

--- _version.py
+++ _version.py
@@ -1,5 +1,5 @@
 """Version information for WAVsToAAF"""
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __author__ = "Jason Brodkey"
 __description__ = "Convert WAV files to Advanced Authoring Format (AAF)"
